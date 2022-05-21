64b8b6f99011488192cc1203730e8a683c5829aa
ahmadzughaer <ahmadzughaer@gmail.com>
2022-05-22 00:28:59 +0300

    refactor the code and add the bouns

--- script/script.js
+++ script/script.js
@@ -1,60 +1,80 @@
 const grid = document.querySelector('.grid');
 const select = document.querySelector("#gridNumber");
-const startButton = document.querySelector("#startBtn")
-let option = document.querySelector('.option')
-let pickedNumber = 5;
-// for (pickedNumber = 1; pickedNumber < 10; pickedNumber++) {
-//     const option = document.createElement("option");
-//     option.text = pickedNumber;
-//     select.appendChild(option);
-// } TODO: make pick mode dynamic 
-
+const statusButton = document.querySelector("#statusButton");
+const buttonText = {
+    start: "start",
+    stop: 'stop'
+}
 
 const createGrid = (rows, cols) => {
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
     for (let i = 0; i < (rows * cols); i++) {
-        let div = document.createElement("div");
+        const div = document.createElement("div");
         grid.appendChild(div).className = "grid-item";
         div.style.background = 'white'
     };
 }
 
-createGrid(pickedNumber, pickedNumber)
-
-startButton.addEventListener('click', (e) => {
-    grid.addEventListener('mouseover', (e) => {
-        console.log(e.target)
-        if (e.target.style.background == "white") {
-            e.target.style.background = "blue";
-        } else if (e.target.style.background == "blue") {
-            e.target.style.background = "white";
+const hoverHandler = (e) => {
+    if (e.target.style.background === "white") {
+        e.target.style.background = "blue";
+    } else if (e.target.style.background === "blue") {
+        e.target.style.background = "white";
+    }
+}
+const startGrid = () => {
+    statusButton.addEventListener('click', (e) => {
+        if (e.target.textContent === buttonText.start) {
+            e.target.textContent = buttonText.stop
+            grid.addEventListener('mouseover', hoverHandler)
+        }
+        else {
+            e.target.textContent = buttonText.start
+            grid.removeEventListener('mouseover', hoverHandler)
         }
     })
-    e.target.textContent = 'stop'
-})
+}
 
-const gridBoard = (color, columnStart, columnEnd) => {
-    let draw;
+
+
+
+const fillGrid = (color, columnStart, columnEnd) => {
     for (let column = columnStart; column <= columnEnd; column++) {
-        draw = grid.childNodes[`${column}`].style.background = color;
+        grid.childNodes[column].style.background = color;
     }
-    return draw
 }
-const resizeBoard = () => {
-    if (pickedNumber > 2) {
-        gridBoard('blue', pickedNumber + 1, pickedNumber + 3)
-        gridBoard('blue', pickedNumber * 2 + 3, pickedNumber * 2 + 3)
 
+const fillInitialGrid = (gridSize) => {
+    if (gridSize > 2) {
+        fillGrid('blue', gridSize + 1, gridSize + 3)
+        fillGrid('blue', gridSize * 2 + 3, gridSize * 2 + 3)
     } else {
-        gridBoard('blue', pickedNumber, pickedNumber)
+        fillGrid('blue', gridSize, gridSize)
     }
 }
-resizeBoard()
 
-// select.addEventListener('change', (e) => {
-//     pickedNumber = e.target.value
-//     createGrid(pickedNumber, pickedNumber)
-// }) TODO: change grid size 
+const getGridSize = () => {
+    return parseInt(select.value, 10)
+}
+
+const initiateGrid = () => {
+    statusButton.textContent = buttonText.start
+    startGrid()
+    createGrid(getGridSize(), getGridSize())
+    fillInitialGrid(getGridSize())
+    select.addEventListener(('change'), () => {
+        grid.innerHTML = ""
+        createGrid(getGridSize(), getGridSize())
+        fillInitialGrid(getGridSize())
+        statusButton.textContent = buttonText.start
+        grid.removeEventListener('mouseover', hoverHandler)
+    
+    })
+}
+
+initiateGrid()
+
+
 
 
